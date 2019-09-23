b2c93b6b132b720da78a80cd43f7ff0667aa8c28
animal35196 <53960880+animal35196@users.noreply.github.com>
2019-09-22 20:31:57 -0500

    adding the last of the comodittys
    i added comonitys so the number of comonitys matched the maximam number of players and changed the minumum players to 3 becuse the game dose not work with 2

--- pitServer.js
+++ pitServer.js
@@ -21,7 +21,7 @@ var server = http.createServer(app).listen(socket,"0.0.0.0",511,function(){conso
 io = io.listen(server);
 /*initializing the websockets communication , server instance has to be sent as the argument */
 
-var minPlayers = 2;
+var minPlayers = 3;
 var maxPlayers = 20;
 
 var allClients = [];
